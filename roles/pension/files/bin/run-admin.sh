989de53545c4f9bd0cff7fff918f416f47d8fbb0
alex misyuk <alex@inqwise.com>
2024-09-29 11:50:12 +0300

    init

---
+++ roles/pension/files/bin/run-admin.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Set directories relative to the app_home directory
+APP_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
+BIN_DIR="$APP_HOME/bin"
+LIB_DIR="$APP_HOME/lib"
+CONFIG_DIR="$APP_HOME/config"
+
+# JVM options for logging and metrics
+LOGGING_OPTS=(
+  "-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory"
+)
+
+# General JVM options
+GENERAL_OPTS=(
+  "-XX:+HeapDumpOnOutOfMemoryError"
+  "-XX:+ShowCodeDetailsInExceptionMessages"
+)
+
+# Module and export options for Java
+MODULE_OPTS=(
+  "--add-modules java.se"
+  "--add-exports java.base/jdk.internal.ref=ALL-UNNAMED"
+  "--add-opens java.base/java.lang=ALL-UNNAMED"
+  "--add-opens java.base/java.nio=ALL-UNNAMED"
+  "--add-opens java.base/sun.nio.ch=ALL-UNNAMED"
+  "--add-opens java.management/sun.management=ALL-UNNAMED"
+  "--add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
+)
+
+# Function to run the Java application
+run_app() {
+  local jar_file=$1
+  local conf_file=$2
+  local log_file=$3
+  local options_file=$4
+  local class_name=$5
+  local jmx_port=$6
+  local jmx_domain=$7
+
+  local metrics_opts=(
+    "-Dvertx.metrics.options.enabled=true"
+    "-Dvertx.metrics.options.jmxEnabled=true"
+    "-Dvertx.metrics.options.jmxDomain=$jmx_domain"
+  )
+
+  local jmx_opts=(
+    "-Dcom.sun.management.jmxremote"
+    "-Dcom.sun.management.jmxremote.port=$jmx_port"
+    "-Dcom.sun.management.jmxremote.local.only=false"
+    "-Dcom.sun.management.jmxremote.authenticate=false"
+    "-Dcom.sun.management.jmxremote.ssl=false"
+  )
+
+  echo "Running $class_name with JMX port $jmx_port and JMX domain $jmx_domain"
+
+  java \
+    ${LOGGING_OPTS[@]} \
+    ${metrics_opts[@]} \
+    ${GENERAL_OPTS[@]} \
+    ${jmx_opts[@]} \
+    ${MODULE_OPTS[@]} \
+    -classpath "$LIB_DIR/*:$jar_file" \
+    io.vertx.core.Launcher run "$class_name" \
+    -options "$options_file" \
+    -conf "$conf_file"
+}
+
+# Move to the app_home directory to execute
+cd "$APP_HOME"
+
+# Run pension-admin
+run_app "$LIB_DIR/pension-admin-fat.jar" \
+        "$CONFIG_DIR/admin.json" \
+        "$CONFIG_DIR/log4j2-admin.xml" \
+        "$CONFIG_DIR/options.json" \
+        "com.tamal.pension.admin.Server" \
+        7777 \
+        "pension-admin"
